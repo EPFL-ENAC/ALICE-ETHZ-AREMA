23da15c5e99f6ab59d8e57718b31ec4dd128e9e1
Pierre Guilbert <pierre.guilbert@epfl.ch>
2023-06-13 09:42:21 +0200

    feat(couchdb-setup): add buildingElements and professionalTypes

--- couchdb-setup/bootstrap/regenerative_materials/_design/professionalType/views/list/map.js
+++ couchdb-setup/bootstrap/regenerative_materials/_design/professionalType/views/list/map.js
@@ -1,6 +1,6 @@
 function (doc) {
   // retrieve only fields necessary for listing professionals
-  if(doc.data && doc.data.type === 'professional') {
+  if(doc.data && doc.data.type === 'professionalType') {
     emit(doc._id, {
       name: doc.name,
     });
