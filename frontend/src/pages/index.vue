03c031e13c4663fd904f69a3540a8b14978186be
Pierre Guilbert <pierre.guilbert@epfl.ch>
2023-06-27 10:33:34 +0200

    feat(frontend): add description for main page

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -2,20 +2,46 @@
 defineOptions({
   name: 'IndexPage',
 })
-
 </script>
 
 <template>
   <div>
-    <!-- <div i-carbon-campsite inline-block text-4xl /> -->
-    <p>
+    <h2 class="title">
       <a rel="noreferrer" href="https://github.com/EPFL-ENAC/alice-ethz-arema" target="_blank">
-        Regenerative map
+        Atlas of regenerative materials
       </a>
-    </p>
-    <p>
-      <em text-sm op75>Regenerative map</em>
-    </p>
+    </h2>
 
+    <p class="mt-2 mb-2">This project aims to be an open repository of knowledge about all currently available resources for sustainable construction.</p>
+    <p class="mt-2 mb-2">
+      This includes information about:
+      <ul>
+        <ol> natural resources, gathered from various scientific or governmental databases, geolocalized,</ol>
+        <ol> building materials made out of the former, including specifications, gathered by experts,</ol>
+        <ol> professionals working in the sector, from architect to craftsman and supplier, with specific geographic information about the latter's sourcing,</ol>
+        <ol> technical construction details explaining how a given building material is to be used for a specific building element (e.g. wall, insulation, finishing...)</ol>
+        <ol> buildings where sustainable construction is implemented, how it is done and where the used materials were sourced.</ol>
+      </ul>
+    </p>
+    <p class="mt-2 mb-2">
+      If available, additional information can be supplemented to the mere practical information such as anthropological, historical and anecdotal tidbits in the shape of text, audio or video content.
+    </p>
+    <p class="mt-2 mb-2">
+      These ambitious but thorough procedure will need the contributions from academic and industrial experts, thus fostering active cooperations and networks both locally and internationally. All data entries will be curated to ensure high quality standards according to the Charta of the project.
+    </p>
+    <p class="mt-2 mb-2">
+      The atlas of regenerative materials will provide academia with valuable teaching support, the construction community with a ready to use database and third parties with additional insight into practices which provide a sustainable and comprehensive alternative to the current depletion of fossil or/and finite resources in the sector.
+    </p>
+    <p class="mt-2 mb-2">
+      Partners:
+      <ul>
+        <ol>
+          Prof. Dr. Guillaume Habert & Shoshana Huber, Chair of sustainable construction, ETH Zürich
+        </ol>
+        <ol>
+          Alia Bengana, Alice Laboratory, EPF Lausanne
+        </ol>
+      </ul>
+    </p>
   </div>
 </template>
