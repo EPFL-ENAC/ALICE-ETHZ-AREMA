56af0a3168a54f7f9643bc0f79152b41920c3386
Pierre Guilbert <pierre.guilbert@epfl.ch>
2023-09-04 13:21:14 +0200

    chore(database): remove couchdb

--- couchdb-setup/bootstrap/regenerative_materials/_design/buildingMaterial/views/list/map.js
+++
@@ -1,9 +0,0 @@
-function (doc) {
-  // retrieve only fields necessary for listing buildingMaterials
-  if(doc.data && doc.data.type === 'buildingMaterial') {
-    emit(doc._id, {
-      name: doc.name,
-    });
-  }
-
-}
