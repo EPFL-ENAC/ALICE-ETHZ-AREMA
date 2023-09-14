61c5be5fa12ba39a153c9099d706a8441a1ad5f9
Guilbert Pierre <guilbep@gmail.com>
2023-09-14 16:23:45 +0200

    fix(api_url): add api_url env

--- frontend/src/feathers-client.ts
+++ frontend/src/feathers-client.ts
@@ -1,29 +1,32 @@
-import { createClient } from '@epfl-enac/arema' // exported by your feathers-api
-import { createPiniaClient } from 'feathers-pinia'
-import socketio from '@feathersjs/socketio-client'
+import { createClient } from '@epfl-enac/arema'; // exported by your feathers-api
+import { createPiniaClient } from 'feathers-pinia';
+import socketio from '@feathersjs/socketio-client';
 // import rest from '@feathersjs/rest-client';
-import io from 'socket.io-client'
+import io from 'socket.io-client';
 
-import { pinia } from './modules/pinia'
+import { pinia } from './modules/pinia';
 
-const host = process.env.API;
-console.log(host)
-if (!host) {
-  throw new Error('process.env.API is not defined')
-}
-const socket = io(host, { transports: ['websocket'], rejectUnauthorized: false })
+const host = process.env.API_URL;
+console.log('THIS IS THE host', host);
+// if (!host) {
+//   throw new Error('process.env.API is not defined')
+// }
+const socket = io(host, {
+  transports: ['websocket'],
+  rejectUnauthorized: false
+});
 // process.env.API == 'http://localhost:3030'
 // const restClient = rest(process.env.API);
 
 export const feathersClient = createClient(
   socketio(socket),
   // restClient.fetch(window.fetch.bind(window)),
-  { storage: window.localStorage })
+  { storage: window.localStorage }
+);
 export const api = createPiniaClient(feathersClient, {
   pinia,
   storage: window.localStorage,
   idField: 'id',
   whitelist: ['$regex'],
   paramsForServer: []
-})
-
+});
