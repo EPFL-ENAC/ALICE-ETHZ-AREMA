04c2939f7f60399d3727f1d518206970e8cc1886
ymarcon <yannick.marcon@epfl.ch>
2023-11-14 10:02:25 +0100

    chore: npm deps updated, code cleaning

--- frontend/src/utils/geocoder.ts
+++ frontend/src/utils/geocoder.ts
@@ -1,5 +1,3 @@
-import { debounce } from 'lodash';
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function handleNominatimResponse(geojson: any): any[] {
   const features = [];
@@ -33,7 +31,6 @@ function handleNominatimResponse(geojson: any): any[] {
 }
 
 let searchController: AbortController;
-let reverseController: AbortController;
 
 /**
  * Example: https://maplibre.org/maplibre-gl-js-docs/example/geocoder/
@@ -68,30 +65,4 @@ export const geocoderApi = {
       features,
     };
   },
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  reverseGeocode: async (config: { query: any }) => {
-    let features = [];
-    try {
-      if (reverseController) reverseController.abort();
-      reverseController = new AbortController();
-      const request = `https://nominatim.openstreetmap.org/reverse?lat=${config.query.lat}&lon=${config.query.lon}&format=geojson&polygon_geojson=1&addressdetails=1`;
-      const response = await fetch(request, {
-        signal: reverseController.signal,
-      });
-      const geojson = await response.json();
-      features = handleNominatimResponse(geojson);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (e: any) {
-      if (e.name !== 'AbortError')
-        console.error(`Failed to reverseGeocode with error: ${e}`);
-    }
-    return {
-      features,
-    };
-  },
-};
-
-export const delayedGeocoderApi = {
-  forwardGeocode: debounce(geocoderApi.forwardGeocode, 500),
-  reverseGeocode: debounce(geocoderApi.reverseGeocode, 500),
 };
