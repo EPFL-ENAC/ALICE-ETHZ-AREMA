e576f25792895d8052425d040e4aa3ad2fbce44b
ymarcon <yannick.marcon@epfl.ch>
2025-01-09 16:46:26 +0100

    feat: link to admin app

--- frontend/src/components/NavDrawer.vue
+++ frontend/src/components/NavDrawer.vue
@@ -1,63 +1,21 @@
 <template>
   <div>
-    <q-btn
-      :disable="home"
-      to="/"
-      no-caps
-      size="48px"
-      :label="$t('home')"
-      class="full-width"
-      align="left"
-    />
+    <q-btn :disable="home" to="/" no-caps size="48px" :label="$t('home')" class="full-width" align="left" />
     <q-separator class="bg-primary" size="2px" />
-    <div
-      style="font-size: 48px; font-weight: 500; line-height: 1.715em"
-      class="q-ml-md q-pt-md"
-    >
+    <div style="font-size: 48px; font-weight: 500; line-height: 1.715em" class="q-ml-md q-pt-md">
       {{ $t('atlas') }}
     </div>
     <div class="row q-mb-md">
-      <q-btn
-        no-caps
-        size="xl"
-        to="/about"
-        :label="$t('about')"
-        class="text-primary"
-      ></q-btn>
+      <q-btn no-caps size="xl" to="/about" :label="$t('about')" class="text-primary"></q-btn>
       <q-icon name="horizontal_rule" size="md" class="q-mt-md" />
-      <q-btn
-        no-caps
-        size="xl"
-        to="/admin"
-        :label="$t('contribute')"
-        class="text-primary"
-      ></q-btn>
+      <q-btn no-caps size="xl" :label="$t('contribute')" class="text-primary" @click="onAdmin"></q-btn>
       <q-icon name="horizontal_rule" size="md" class="q-mt-md" />
-      <q-btn
-        no-caps
-        size="xl"
-        to="/search"
-        :label="$t('search')"
-        class="text-primary"
-      ></q-btn>
+      <q-btn no-caps size="xl" to="/search" :label="$t('search')" class="text-primary"></q-btn>
       <q-icon name="horizontal_rule" size="md" class="q-mt-md" />
-      <q-btn
-        no-caps
-        size="xl"
-        to="/videos"
-        :label="$t('videos')"
-        class="text-primary"
-      ></q-btn>
+      <q-btn no-caps size="xl" to="/videos" :label="$t('videos')" class="text-primary"></q-btn>
     </div>
     <q-separator class="bg-primary" size="2px" />
-    <q-btn
-      to="/contact"
-      no-caps
-      size="48px"
-      :label="$t('contact')"
-      class="full-width"
-      align="left"
-    />
+    <q-btn to="/contact" no-caps size="48px" :label="$t('contact')" class="full-width" align="left" />
   </div>
 </template>
 
@@ -66,4 +24,8 @@ interface Props {
   home?: boolean;
 }
 defineProps<Props>();
+
+function onAdmin() {
+  window.location.href = '/admin/';
+}
 </script>
