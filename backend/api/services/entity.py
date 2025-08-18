6536d3143fe6a8c5b32e018134b24eb58f3d399b
ymarcon <yannick.marcon@epfl.ch>
2025-08-18 08:37:36 +0200

    feat: check for roles and authorship before state transitions

--- backend/api/services/entity.py
+++ backend/api/services/entity.py
@@ -34,55 +34,79 @@ class EntityService:
         }
         set_state_func = switcher.get(state)
         if set_state_func:
-            return set_state_func(entity)
+            return set_state_func(entity, user)
         else:
             raise HTTPException(
                 status_code=400, detail=f"State change to {state} not allowed")
 
-    def _set_in_review(self, entity: Entity) -> Entity:
+    def _set_in_review(self, entity: Entity, user: User) -> Entity:
         """Change the state of the entity to in-review"""
         if entity.state == "in-review":
             return entity
         if entity.state != "draft" and entity.state != "to-publish":
             raise HTTPException(
                 status_code=400, detail="Entity must be in draft or to-publish state to be reviewed")
+        if self._is_contributor(user) and not self._is_author(entity, user):
+            raise HTTPException(
+                status_code=402, detail="Operation not allowed")
         entity.state = "in-review"
         return entity
 
-    def _set_to_publish(self, entity: Entity) -> Entity:
+    def _set_to_publish(self, entity: Entity, user: User) -> Entity:
         """Change the state of the entity to to-publish"""
         if entity.state == "to-publish":
             return entity
         if entity.state != "in-review":
             raise HTTPException(
                 status_code=400, detail="Entity must be in review to be published")
+        if self._is_contributor(user):
+            raise HTTPException(
+                status_code=402, detail="Operation not allowed")
         entity.state = "to-publish"
         return entity
 
-    def _set_to_unpublish(self, entity: Entity) -> Entity:
+    def _set_to_unpublish(self, entity: Entity, user: User) -> Entity:
         """Change the state of the entity to to-unpublish"""
         if entity.state == "to-unpublish":
             return entity
+        if self._is_contributor(user) and not self._is_author(entity, user):
+            raise HTTPException(
+                status_code=402, detail="Operation not allowed")
         entity.state = "to-unpublish"
         return entity
 
-    def _set_draft(self, entity: Entity) -> Entity:
+    def _set_draft(self, entity: Entity, user: User) -> Entity:
         """Change the state of the entity to draft"""
         if entity.state == "draft":
             return entity
+        if self._is_contributor(user) and not self._is_author(entity, user):
+            raise HTTPException(
+                status_code=402, detail="Operation not allowed")
         entity.state = "draft"
         return entity
 
-    def _set_locked(self, entity: Entity) -> Entity:
+    def _set_locked(self, entity: Entity, user: User) -> Entity:
         """Change the state of the entity to locked"""
         if entity.state == "locked":
             return entity
+        if "app-administrator" not in user.realm_roles:
+            raise HTTPException(
+                status_code=402, detail="Operation not allowed")
         entity.state = "locked"
         return entity
 
-    def _set_to_delete(self, entity: Entity) -> Entity:
+    def _set_to_delete(self, entity: Entity, user: User) -> Entity:
         """Change the state of the entity to to-delete"""
         if entity.state == "to-delete":
             return entity
+        if self._is_contributor(user) and not self._is_author(entity, user):
+            raise HTTPException(
+                status_code=402, detail="Operation not allowed")
         entity.state = "to-delete"
         return entity
+
+    def _is_contributor(user: User) -> bool:
+        return "app-contributor" in user.realm_roles
+
+    def _is_author(self, entity: Entity, user: User) -> bool:
+        return entity.created_by == user.username or entity.updated_by == user.username
