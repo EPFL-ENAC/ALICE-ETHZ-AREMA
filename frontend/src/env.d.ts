f33cbc13ebe5c63d753e28fba25c99196ff8fedd
Yannick Marcon <yannick.marcon@epfl.ch>
2026-02-05 09:06:12 +0100

    feat: added martin as the vector tiles server in place of geojson files from CDN (#170)
    * feat: added martin as the vector tiles server in place of geojson files from CDN
    
    * feat: updated corn layer from Mais_2024 catalog to Corn_CH_FR_2024
    
    * chore: updated data gitignore
    
    * chore: added data files to git lfs
    
    * feat: local gis services are optional, default points to dev's martin
    
    * chore: updated corn map
    
    * chore: renamed Reynoutria_Japonica_2025_2024.geojson
    
    * chore: added dev notes in README

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -4,5 +4,7 @@ declare namespace NodeJS {
     VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
     VUE_ROUTER_BASE: string | undefined;
     API_URL: string | undefined;
+    API_PATH: string | undefined;
+    MARTIN_URL: string | undefined;
   }
 }
